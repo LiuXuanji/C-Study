317f3b3b1213c3645b0ec7bc0c077964089a5f09
Hydrate-Liu <93075533+LiuXuanji@users.noreply.github.com>
2022-04-13 14:24:59 +0800

    Add files via upload

---
+++ C++ 2.1_myfist/C++ 2.1_myfist/myfirst.cpp
@@ -0,0 +1,12 @@
+// myfirst.cpp -- displays a message
+
+#include<iostream>                             // a preprocessor directive
+using namespace std;                           // make definitions visible
+
+int main()                                     // function header
+{                                              // start of function body
+	cout << " Come up and C++ me some time. "; // message
+	cout << endl;                              // start a new line
+	cout << " You won't regret it! " << endl;  // more output 
+	return 0;                                  // terminate main()
+}
